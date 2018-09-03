a85ceec9a90268ac89c2e0a61d84e65ff68934c6
DESKTOP-H201P5Q\Carlos Átila <tilino@gmail.com>
2018-09-03 16:52:13 +1000

    Cleaning code

--- ShortUrl/Controllers/ShortUrlController.cs
+++ ShortUrl/Controllers/ShortUrlController.cs
@@ -17,38 +17,7 @@ namespace ShortUrl.Controllers
         public ShortUrlController(UrlContext context)
         {
             _context = context;
-        }
-        //private static string[] Summaries = new[]
-        //{
-        //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        //};
-
-        //[HttpGet("[action]")]
-        //public IEnumerable<WeatherForecast> WeatherForecasts()
-        //{
-        //    var rng = new Random();
-        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        //    {
-        //        DateFormatted = DateTime.Now.AddDays(index).ToString("d"),
-        //        TemperatureC = rng.Next(-20, 55),
-        //        Summary = Summaries[rng.Next(Summaries.Length)]
-        //    });
-        //}
-
-        //public class WeatherForecast
-        //{
-        //    public string DateFormatted { get; set; }
-        //    public int TemperatureC { get; set; }
-        //    public string Summary { get; set; }
-
-        //    public int TemperatureF
-        //    {
-        //        get
-        //        {
-        //            return 32 + (int)(TemperatureC / 0.5556);
-        //        }
-        //    }
-        //}
+        }        
 
         [HttpPost("api/[controller]/[action]")]
         public async Task<IActionResult> Shorten(String uriList)
@@ -109,12 +78,6 @@ namespace ShortUrl.Controllers
             }
             return Redirect(_context.Urls.Where(u => u.Token == token).First().LongUrl);
         }
-
-        //[HttpGet("[action]")]
-        //public IEnumerable<Url> GetUrls()
-        //{
-        //    return _context.Urls;
-        //}
 
         private static Random random = new Random();
         public static string RandomString()
